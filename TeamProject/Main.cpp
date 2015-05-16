7a0b734f62d2aecaa92648df624c195d59f6032d
Tuan Pham <tt.pham1991+git@gmail.com>
2015-05-16 09:59:51 -0700

    Created VS Project with original files

---
+++ TeamProject/Main.cpp
@@ -0,0 +1,23 @@
+// main.cpp
+// Author: Cameron March
+
+// Remove if not using Visual Studio
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include "Account.h"
+#include "Storage.h"
+
+const double STARTING_BALANCE = 100.00;
+const double ANNUAL_INTEREST_RATE = 2.3;
+
+int main()
+{
+	Account account(STARTING_BALANCE, ANNUAL_INTEREST_RATE);
+
+	Storage storage("deleteMe.txt");
+
+	storage.save(account);
+
+	return 0;
+}
