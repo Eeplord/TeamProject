6574ab2dc273b3ad0f531b178120c0dadcf55d41
neriov <neriov@users.noreply.github.com>
2015-06-03 07:53:36 -0700

    Update Id.h

--- TeamProject/Id.h
+++ TeamProject/Id.h
@@ -1,22 +1,15 @@
+// Id.h
 #ifndef ID_H
 #define ID_H
 
-class Id {
-private:
+class Id
+{
+	private:
+		int idGenerator_;
+		int id_;
 
-	// Generates an id for each new account
-	static int idGenerator_ = 0;
-
-	// The id of the account
-	int id_;
-
-public:
-
-	// Set id_ = idGenerator++ everytime it's called
-	Id();
-
-	// Returns id_ of the account
-	int getId();
-}
-
-#endif
+	public:
+		Id(); //will generate 4 digit id's with increments of 1
+        	int getId();
+};
+#endif // ID_H
